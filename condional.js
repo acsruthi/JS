83365e1367131b3525e9c72213fe34a2ecadcdcc
Sruthi Candoth <acsruthi88@gmail.com>
2014-12-10 21:47:44 +1000

    Conditional statements

---
+++ condional.js
@@ -0,0 +1,26 @@
+ var x=6;
+
+ if(x>3)
+
+   console.log("True");
+
+ else
+    console.log("False");
+
+while(x<=12)
+  {
+   console.log(x);
+   x++;
+  }
+
+var result=1;
+var c=0;
+
+while(c<10)
+  {
+    result=result*2;
+    c++;
+  }
+  console.log(result);
+
+  
