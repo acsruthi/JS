a81c5ae6254dd31d7653f41cc51de471e18685d8
Sruthi Candoth <acsruthi88@gmail.com>
2014-12-17 12:45:55 +1000

    Comprison function

---
+++ deepComp.js
@@ -0,0 +1,27 @@
+function deepEqual(a,b){
+
+  if(typeof(a)===typeof(b) && a.length===b.length)
+    {
+      var i=0;
+      while(i<a.length)
+      {
+        if(a[i]===b[i])
+           i++;
+ 	
+        else
+         return false;
+	   
+
+      }
+
+      return true;
+    }
+
+
+
+
+  else
+    return false;
+};
+
+console.log(deepEqual("123",123));
