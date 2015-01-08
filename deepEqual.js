09317bc05e028e724e9d4b30833bc2144bb103ea
Sunil Thaha <sun.il.th.aha@gmail.com>
2015-01-08 11:50:20 +1000

    Fixes whitespace

--- deepEqual.js
+++ deepEqual.js
@@ -1,29 +1,31 @@
 'use strict';
 
 function deepEqual(a, b) {
-  if (a === b)
-    return true;
-
-  if (a == null || typeof a != "object" || b == null || typeof b != "object")
-    return false;
-
-  var propsInA = 0, propsInB = 0;
-
-  for (var prop in a) {
-    propsInA ++;
-    console.log (prop, propsInA);
-  }
-
-  for (var prop in b) {
-    propsInB ++;
-    if (!(prop in a) || !(deepEqual (a[prop], b[prop]))) {
-      return false;
+    if (a === b) {
+        return true;
     }
-  }
 
-  return propsInA == propsInB;
-};
+    if (a == null || typeof a != "object" || b == null || typeof b != "object") {
+        return false;
+    }
+
+    var propsInA = 0, propsInB = 0;
+
+    for (var prop in a) {
+        propsInA ++;
+        console.log (prop, propsInA);
+    }
+
+    for (var prop in b) {
+        propsInB ++;
+        if (!(prop in a) || !(deepEqual (a[prop], b[prop]))) {
+            return false;
+        }
+    }
+
+    return propsInA == propsInB;
+}
 
 
-var obj = {here: {is: "an"}, object: 2};
+var obj = {here: {is: 'an' }, object: 2};
 console.log(deepEqual(obj, {here: 1, object: 2}));
--- ex2.1.js
+++ ex2.1.js
@@ -1,18 +1,19 @@
 'use strict';
  // Looping a triangle
 for (var i = "#"; i.length <= 7; i += "#") {
-    console.log (i);
+    console.log(i);
   }
 
 for (var j = "*"; j.length <= 4; j += "*") {
-      console.log (j);
+      console.log(j);
     }
 
-console.log ( "Sruthi" .charAt (5));
+console.log("Sruthi".charAt(5));
 
 
 var array = [1 , 2 , 3];
-for ( var i = 0; i < array . length ; i ++) {
-  var current = array [ i ];
-  console . log ( current ) ;
+
+for ( var i = 0; i < array.length; i++) {
+  var current = array[ i ];
+  console.log(current) ;
 }
